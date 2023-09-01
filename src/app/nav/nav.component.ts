f1d25354716c6bd252a709124ee27962edf5aa96
Marco Vasquez <39408535+mvasquez1-ksu@users.noreply.github.com>
2023-08-31 20:39:33 -0500

    commit

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements AfterViewInit{
 
+  ngAfterViewInit(): void {
+
+  }
+
+  openmenu(id: string) {
+    var element = document.getElementById(id)!;
+    element.style.width = "50%";
+    element.style.fontSize = "2.5vw";
+    //this.nav_item!.style.width = "90%";
+    console.warn("clicked");
+  }
 }
